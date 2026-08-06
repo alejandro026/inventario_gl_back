e55d26a90e4405e3b8e9d9ea38426f1c2eb6ff9a
alejandro026 <alejandro02262@gmail.com>
2026-08-06 13:38:21 -0600

    Fix import

--- src/main/java/com/guerrero/Inventario/dto/PagedResponse.java
+++ src/main/java/com/guerrero/Inventario/dto/PagedResponse.java
@@ -1,6 +1,10 @@
 package com.guerrero.Inventario.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.domain.Page;
 import java.util.List;
 
